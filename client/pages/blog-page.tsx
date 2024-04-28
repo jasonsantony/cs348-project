87a4de0dcc0ce62eeb1218214be44218ab27f7f4
jasonsantony <42359339+jasonsantony@users.noreply.github.com>
2024-04-28 16:59:55 -0400

    get ready to submit reviews

--- client/pages/blog-page.tsx
+++ client/pages/blog-page.tsx
@@ -5,7 +5,12 @@ import {
   Paper,
   RingProgress,
   PaperProps,
+  Modal,
+  Button,
+  Slider,
 } from "@mantine/core";
+
+import { useDisclosure } from "@mantine/hooks";
 
 import { RichTextEditor, Link } from "@mantine/tiptap";
 import { useEditor } from "@tiptap/react";
@@ -17,7 +22,7 @@ import Superscript from "@tiptap/extension-superscript";
 import SubScript from "@tiptap/extension-subscript";
 
 import classes from "../styles/review.module.css";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { format, formatDistanceToNow, parseISO } from "date-fns";
 import parse from "html-react-parser";
 
@@ -32,18 +37,16 @@ type ReviewJson = {
 
 export default function BlogPage(props: PaperProps) {
   const [reviews, setReviews] = useState<ReviewJson[]>([]);
-  const [blogPostUsername, setBlogPostUsername] = useState<string>("");
+  const [blogPageUsername, setBlogPageUsername] = useState<string>("");
   const [username, setUsername] = useState<string>("");
   useEffect(() => {
-    setBlogPostUsername(localStorage.getItem("blogPageUsername") || "");
+    setBlogPageUsername(localStorage.getItem("blogPageUsername") || "");
   }, []);
-
   useEffect(() => {
     setUsername(localStorage.getItem("username") || "");
   }, []);
 
   const [blogBio, setBlogBio] = useState("");
-
   useEffect(() => {
     fetch(
       `http://localhost:8080/api/user/${localStorage.getItem(
@@ -55,7 +58,11 @@ export default function BlogPage(props: PaperProps) {
   }, []);
 
   useEffect(() => {
-    fetch(`http://localhost:8080/api/user/${blogPostUsername}/reviews`)
+    fetch(
+      `http://localhost:8080/api/user/${localStorage.getItem(
+        "blogPageUsername"
+      )}/reviews`
+    )
       .then((response) => response.json())
       .then((data) => {
         setReviews(data);
@@ -66,7 +73,7 @@ export default function BlogPage(props: PaperProps) {
   }, []);
 
   const [bio, setBio] = useState("");
-  function Editor() {
+  function BioEditor() {
     useEffect(() => {
       fetch(
         `http://localhost:8080/api/user/${localStorage.getItem("username")}/bio`
@@ -189,31 +196,175 @@ export default function BlogPage(props: PaperProps) {
     const readableTimestamp = format(parseISO(timestamp), "PPpp");
 
     return (
-      <Group>
-        <div style={{ position: "relative", width: 80, height: 80 }}>
-          <RingProgress
-            sections={[{ value: rating_value, color: color }]}
-            roundCaps
-            size={80}
-            label={
-              <Text c={color} fw={700} ta="center" size="xl">
-                {rating_value}
-              </Text>
-            }
-          />
-        </div>
-        <div>
-          <Text fz="xl">{show_title}</Text>
-          <Text fz="xs">
-            Directed by {show_director}, {show_release_year}
-          </Text>
-          <Text fz="xs" c="dimmed" title={readableTimestamp}>
-            Reviewed {timeSinceReview} ago
-          </Text>
-        </div>
-      </Group>
+      <Paper
+        withBorder
+        radius="md"
+        className={classes.comment}
+        style={{ overflow: "auto" }}
+      >
+        <Group>
+          <div style={{ position: "relative", width: 80, height: 80 }}>
+            <RingProgress
+              sections={[{ value: rating_value, color: color }]}
+              roundCaps
+              size={80}
+              label={
+                <Text c={color} fw={700} ta="center" size="xl">
+                  {rating_value}
+                </Text>
+              }
+            />
+          </div>
+          <div>
+            <Text fz="xl">{show_title}</Text>
+            <Text fz="xs">
+              Directed by {show_director}, {show_release_year}
+            </Text>
+            <Text fz="xs" c="dimmed" title={readableTimestamp}>
+              Reviewed {timeSinceReview} ago
+            </Text>
+          </div>
+        </Group>
+        <TypographyStylesProvider className={classes.body}>
+          <div>{parse(review_text)}</div>
+        </TypographyStylesProvider>
+      </Paper>
     );
   };
+
+  function WriteReview() {
+    const [opened, { open, close }] = useDisclosure(false);
+    const [reviewContent, setReviewContent] = useState("");
+    const [sliderValue, setSliderValue] = useState(5);
+
+    const editor = useEditor({
+      extensions: [
+        StarterKit,
+        Underline,
+        Link,
+        Superscript,
+        SubScript,
+        Highlight,
+        TextAlign.configure({ types: ["heading", "paragraph"] }),
+      ],
+      content: "",
+      onUpdate: () => {
+        if (editor) {
+          setReviewContent(editor.getHTML());
+        }
+      },
+    });
+
+    const submitReview = async () => {
+      // use the reviewContent variable to submit the content of the editor
+
+      console.log(reviewContent, sliderValue);
+      close();
+    };
+
+    function ReviewSlider() {
+      const marks = [
+        { value: 0, label: "0" },
+        { value: 1, label: "1" },
+        { value: 2, label: "2" },
+        { value: 3, label: "3" },
+        { value: 4, label: "4" },
+        { value: 5, label: "5" },
+        { value: 6, label: "6" },
+        { value: 7, label: "7" },
+        { value: 8, label: "8" },
+        { value: 9, label: "9" },
+        { value: 10, label: "10" },
+      ];
+      return (
+        <>
+          <Slider
+            value={sliderValue}
+            onChange={(val) => setSliderValue(val)}
+            min={0}
+            max={10}
+            label={(val) => marks.find((mark) => mark.value === val)!.label}
+            step={1}
+            marks={marks}
+            styles={{
+              markLabel: { display: "none" },
+              root: { margin: "40px 0" },
+            }}
+          />
+        </>
+      );
+    }
+
+    return (
+      <>
+        <Modal
+          opened={opened}
+          onClose={close}
+          title="Write a Review"
+          size="87.5%"
+          radius={0}
+          yOffset="15%"
+          transitionProps={{ transition: "fade", duration: 200 }}
+        >
+          <RichTextEditor
+            editor={editor}
+            className="h-full overflow-auto w-full"
+          >
+            <RichTextEditor.Toolbar sticky stickyOffset={0}>
+              <RichTextEditor.ControlsGroup>
+                <RichTextEditor.Bold />
+                <RichTextEditor.Italic />
+                <RichTextEditor.Underline />
+                <RichTextEditor.Strikethrough />
+                <RichTextEditor.ClearFormatting />
+                <RichTextEditor.Highlight />
+                <RichTextEditor.Code />
+              </RichTextEditor.ControlsGroup>
+
+              <RichTextEditor.ControlsGroup>
+                <RichTextEditor.H1 />
+                <RichTextEditor.H2 />
+                <RichTextEditor.H3 />
+                <RichTextEditor.H4 />
+              </RichTextEditor.ControlsGroup>
+
+              <RichTextEditor.ControlsGroup>
+                <RichTextEditor.Blockquote />
+                <RichTextEditor.Hr />
+                <RichTextEditor.BulletList />
+                <RichTextEditor.OrderedList />
+                <RichTextEditor.Subscript />
+                <RichTextEditor.Superscript />
+              </RichTextEditor.ControlsGroup>
+
+              <RichTextEditor.ControlsGroup>
+                <RichTextEditor.Link />
+                <RichTextEditor.Unlink />
+              </RichTextEditor.ControlsGroup>
+
+              <RichTextEditor.ControlsGroup>
+                <RichTextEditor.AlignLeft />
+                <RichTextEditor.AlignCenter />
+                <RichTextEditor.AlignJustify />
+                <RichTextEditor.AlignRight />
+              </RichTextEditor.ControlsGroup>
+
+              <RichTextEditor.ControlsGroup>
+                <RichTextEditor.Undo />
+                <RichTextEditor.Redo />
+              </RichTextEditor.ControlsGroup>
+            </RichTextEditor.Toolbar>
+
+            <RichTextEditor.Content />
+          </RichTextEditor>
+          <ReviewSlider />
+          <Button onClick={submitReview}>Submit Review</Button>
+        </Modal>
+
+        <Button onClick={open}>Write a Review</Button>
+      </>
+    );
+  }
 
   return (
     <div
@@ -228,7 +379,7 @@ export default function BlogPage(props: PaperProps) {
     >
       <div>
         <h1 style={{ fontSize: "2em", fontWeight: "bold" }}>
-          Welcome to {blogPostUsername}'s Page
+          Welcome to {blogPageUsername}'s Page
         </h1>
       </div>
       <Paper
@@ -240,12 +391,12 @@ export default function BlogPage(props: PaperProps) {
           width: "100%",
           height: "20vh",
           marginBottom: "1em", // Add some space between the papers
-          overflow: username === blogPostUsername ? "hidden" : "auto",
+          overflow: username === blogPageUsername ? "hidden" : "auto",
           position: "relative",
         }}
       >
         <TypographyStylesProvider className={classes.body}>
-          {username === blogPostUsername ? (
+          {username === blogPageUsername ? (
             <div
               style={{
                 height: "100%",
@@ -263,7 +414,7 @@ export default function BlogPage(props: PaperProps) {
                   right: 10, // Add this line
                 }}
               >
-                <Editor />
+                <BioEditor />
               </div>
             </div>
           ) : blogBio ? (
@@ -286,11 +437,20 @@ export default function BlogPage(props: PaperProps) {
         style={{
           maxWidth: "87.5%",
           width: "100%",
-          height: "50vh",
+          height: "60vh",
           overflow: "auto",
         }}
       >
         <TypographyStylesProvider className={classes.body}>
+          <div
+            style={{
+              display: "flex",
+              marginTop: "-10px",
+              marginBottom: "20px",
+            }}
+          >
+            {blogPageUsername === username ? <WriteReview /> : null}
+          </div>
           {reviews.length === 0 ? (
             <Text fz="md" c="dimmed">
               User has no reviews.
